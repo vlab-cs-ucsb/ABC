d220d0874f1b3dde453595aa350adb2c8d83822a
baki <baki@cs.ucsb.edu>
2015-04-21 12:58:05 -0700

    Add autogen files for external projects

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-libtoolize && aclocal && automake --gnu --add-missing && autoconf 
+libtoolize && aclocal && automake --gnu --add-missing && autoconf
+
+cp autogen.sh.glog external/glog/autogen.sh
+cp autogen.sh.googletest external/glog/autogen.sh 
 
