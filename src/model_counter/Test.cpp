074775ab2abea2d2b19df2da6207abc484a748f5
baki <baki@cs.ucsb.edu>
2015-12-14 16:10:56 -0800

    Add initial model counting configuration

---
+++ src/model_counter/Test.cpp
@@ -0,0 +1,23 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: Oct 21, 2015
+ *      Author: lucas
+ */
+
+#include "Test.h"
+
+namespace Vlab {
+namespace ModelCounter {
+
+Test::Test() {
+	// TODO Auto-generated constructor stub
+	std::cout<<"Hello" << std::endl;
+}
+
+Test::~Test() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ModelCounter */
+} /* namespace Vlab */
---
+++ src/model_counter/Test.h
@@ -0,0 +1,24 @@
+/*
+ * Test.h
+ *
+ *  Created on: Oct 21, 2015
+ *      Author: lucas
+ */
+
+#ifndef SRC_MODEL_COUNTER_TEST_H_
+#define SRC_MODEL_COUNTER_TEST_H_
+#include <iostream>
+
+namespace Vlab {
+namespace ModelCounter {
+
+class Test {
+public:
+	Test();
+	virtual ~Test();
+};
+
+} /* namespace ModelCounter */
+} /* namespace Vlab */
+
+#endif /* SRC_MODEL_COUNTER_TEST_H_ */
