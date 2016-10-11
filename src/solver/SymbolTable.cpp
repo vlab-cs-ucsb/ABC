db250a8122dadad2f2805389c7ecbbab73426bbb
baki <baki@cs.ucsb.edu>
2016-10-10 17:49:59 -0700

    Mark seg fault for future reference to fix it

--- src/solver/SymbolTable.cpp
+++ src/solver/SymbolTable.cpp
@@ -19,6 +19,7 @@ SymbolTable::SymbolTable()
 }
 
 SymbolTable::~SymbolTable() {
+  LOG(FATAL)<< "fix seg fault";
   for (auto& map_pair : variable_value_table) {
     for (auto& value_pair : map_pair.second) {
       delete value_pair.second;
