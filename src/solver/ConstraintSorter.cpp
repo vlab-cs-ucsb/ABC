2380ecffc3a9383be6921968c5bc7c92b3e88ab7
William Eiers <snkzny@gmail.com>
2016-10-13 10:29:35 -0700

    After merge with cool_string

--- src/solver/ConstraintSorter.cpp
+++ src/solver/ConstraintSorter.cpp
@@ -619,69 +619,10 @@ ConstraintSorter::TermNode_ptr ConstraintSorter::process_child_nodes(TermNode_pt
 }
 
 void ConstraintSorter::sort_terms(std::vector<TermNode_ptr>& term_node_list) {
-//  std::vector<TermNode_ptr> sorted_term_node_list;
-//
-//  for (auto it = term_node_list.begin(); it != term_node_list.end(); ) {
-//    if ((*it)->numOfTotalVars() == 0) {
-//      sorted_term_node_list.push_back((*it));
-//      it = term_node_list.erase(it);
-//    } else {
-//      it++;
-//    }
-//  }
-//
-//  std::sort(term_node_list.begin(), term_node_list.end(),
-//          [](TermNode_ptr left_node, TermNode_ptr right_node) -> bool {
-//            return (left_node->numOfTotalVars() < right_node->numOfTotalVars());
-//          });
-//
-//  for (auto it = term_node_list.begin(); it != term_node_list.end(); ) {
-//    if (not (*it)->hasSymbolicVar()) {
-//      sorted_term_node_list.push_back((*it));
-//      it = term_node_list.erase(it);
-//    } else {
-//      it++;
-//    }
-//  }
-//
-//  term_node_list.insert(term_node_list.begin(), sorted_term_node_list.begin(), sorted_term_node_list.end());
-//
-///**
-// * TODO Apply better heuristic to break dependency cycles
-// * while term node list is not empty do that again and again
-// */
-////  int num_of_variable_counter = 1;
-////  while (not term_node_list.empty()) {
-////
-////    std::queue<TermNode_ptr> work_list;
-////    for (auto it = term_node_list.begin(); it != term_node_list.end(); ) {
-////      if ((*it)->numOfTotalVars() == num_of_variable_counter) {
-////        work_list.push(*it);
-////        it = term_node_list.erase(it);
-////      } else {
-////        it++;
-////      }
-////
-////      while (not work_list.empty()) {
-////        TermNode_ptr curr_term_node = work_list.front(); work_list.pop();
-////
-////      }
-////    }
-////
-////
-////    num_of_variable_counter++;
-////  }
-//
-//  DVLOG(VLOG_LEVEL) << "node list sorted";
-
   std::vector<TermNode_ptr> sorted_term_node_list;
-  std::vector<TermNode_ptr> concat_node_list;
 
   for (auto it = term_node_list.begin(); it != term_node_list.end(); ) {
     if ((*it)->numOfTotalVars() == 0) {
-      sorted_term_node_list.push_back((*it));
-      it = term_node_list.erase(it);
-    } else if(not (*it)->hasSymbolicVar()) {
       sorted_term_node_list.push_back((*it));
       it = term_node_list.erase(it);
     } else {
@@ -703,10 +644,42 @@ void ConstraintSorter::sort_terms(std::vector<TermNode_ptr>& term_node_list) {
     }
   }
 
-  //term_node_list.insert(term_node_list.begin(), concat_node_list.begin(), concat_node_list.end());
   term_node_list.insert(term_node_list.begin(), sorted_term_node_list.begin(), sorted_term_node_list.end());
-
   DVLOG(VLOG_LEVEL) << "node list sorted";
+
+//  std::vector<TermNode_ptr> sorted_term_node_list;
+//  std::vector<TermNode_ptr> concat_node_list;
+//
+//  for (auto it = term_node_list.begin(); it != term_node_list.end(); ) {
+//    if ((*it)->numOfTotalVars() == 0) {
+//      sorted_term_node_list.push_back((*it));
+//      it = term_node_list.erase(it);
+//    } else if(not (*it)->hasSymbolicVar()) {
+//      sorted_term_node_list.push_back((*it));
+//      it = term_node_list.erase(it);
+//    } else {
+//      it++;
+//    }
+//  }
+//
+//  std::sort(term_node_list.begin(), term_node_list.end(),
+//          [](TermNode_ptr left_node, TermNode_ptr right_node) -> bool {
+//            return (left_node->numOfTotalVars() < right_node->numOfTotalVars());
+//          });
+//
+//  for (auto it = term_node_list.begin(); it != term_node_list.end(); ) {
+//    if (not (*it)->hasSymbolicVar()) {
+//      sorted_term_node_list.push_back((*it));
+//      it = term_node_list.erase(it);
+//    } else {
+//      it++;
+//    }
+//  }
+//
+//  //term_node_list.insert(term_node_list.begin(), concat_node_list.begin(), concat_node_list.end());
+//  term_node_list.insert(term_node_list.begin(), sorted_term_node_list.begin(), sorted_term_node_list.end());
+//
+//  DVLOG(VLOG_LEVEL) << "node list sorted";
 
 }
 
