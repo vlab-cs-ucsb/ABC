678b5736c281542baa1a05acf0b917928ad500ff
baki <baki@cs.ucsb.edu>
2016-11-10 15:38:02 -0800

    Fix serialization bug on Eigen

--- src/utils/Serialize.h
+++ src/utils/Serialize.h
@@ -70,35 +70,92 @@ void load(Archive& ar, Eigen::SparseVector<Theory::BigInteger>& sparse_vector) {
 
 template<class Archive>
 void save(Archive& ar, const Eigen::SparseMatrix<Theory::BigInteger>& sparse_matrix) {
-  ar(sparse_matrix.rows());
-  ar(sparse_matrix.cols());
-  ar(sparse_matrix.outerSize());
-  ar(sparse_matrix.innerSize());
-  ar(sparse_matrix.nonZeros());
-  for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i < sparse_matrix.nonZeros(); ++i) {
+//  std::cout << std::left;
+//  std::cout << std::setw(20) << "sizeof(int):" << sizeof(int) << std::endl;
+//  std::cout << std::setw(20) << "sizeof(Index):" << sizeof(Eigen::SparseMatrix<Theory::BigInteger>::Index) << std::endl;
+//  std::cout << std::setw(20) << "sizeof(StorageIndex):" << sizeof(Eigen::SparseMatrix<Theory::BigInteger>::StorageIndex) << std::endl;
+//  std::cout << std::setw(20) << "sizeof(size_t):" << sizeof(std::size_t) << std::endl;
+//  std::cout << "--------------------------------------------" << std::endl;
+//
+//  std::cout << std::setw(20) << "rows - cols:" << sparse_matrix.rows() << " - " << sparse_matrix.cols() << std::endl;
+//  std::cout << std::setw(20) << "non zeros:" << sparse_matrix.nonZeros() << std::endl;
+//  std::cout << std::setw(20) << "outer size:" << sparse_matrix.outerSize() << std::endl;
+//  std::cout << std::setw(20) << "inner size:" << sparse_matrix.innerSize() << std::endl;
+//  std::cout << "values:" << std::endl;
+//  for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i < sparse_matrix.nonZeros(); ++i) {
+//    std::cout << sparse_matrix.valuePtr()[i] << " ";
+//  }
+//  std::cout << std::endl;
+//  std::cout << "outer pointers:" << std::endl;
+//  for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i <= sparse_matrix.outerSize(); ++i) {
+//    std::cout << sparse_matrix.outerIndexPtr()[i] << ",";
+//    std::cout << sparse_matrix.derived().outerIndexPtr()[i] << "  ";
+//  }
+//  std::cout << std::endl;
+//  std::cout << "inner pointers:" << std::endl;
+//  for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i < sparse_matrix.nonZeros(); ++i) {
+//    std::cout << sparse_matrix.innerIndexPtr()[i] << ",";
+//    std::cout << sparse_matrix.derived().innerIndexPtr()[i] << " ";
+//  }
+//  std::cout << std::endl;
+//  std::cout << "------------------------------------------------" << std::endl;
+
+  Eigen::SparseMatrix<Theory::BigInteger>::Index rows = sparse_matrix.rows(), cols = sparse_matrix.cols(),
+      non_zeros = sparse_matrix.nonZeros(), outer_size = sparse_matrix.outerSize(), inner_size = sparse_matrix.innerSize();
+  ar(rows);
+  ar(cols);
+  ar(non_zeros);
+  ar(outer_size);
+  ar(inner_size);
+  for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i < non_zeros; ++i) {
     Util::Serialize::save(ar, sparse_matrix.valuePtr()[i]);
   }
-  ar(cereal::binary_data(sparse_matrix.outerIndexPtr(), sparse_matrix.outerSize() * sizeof(Eigen::SparseMatrix<Theory::BigInteger>::StorageIndex)));
-  ar(cereal::binary_data(sparse_matrix.innerIndexPtr(), sparse_matrix.nonZeros() * sizeof(Eigen::SparseMatrix<Theory::BigInteger>::StorageIndex)));
+  ar(cereal::binary_data(sparse_matrix.outerIndexPtr(), (outer_size + 1) * sizeof(Eigen::SparseMatrix<Theory::BigInteger>::StorageIndex)));
+  ar(cereal::binary_data(sparse_matrix.innerIndexPtr(), non_zeros * sizeof(Eigen::SparseMatrix<Theory::BigInteger>::StorageIndex)));
 }
 
 template<class Archive>
 void load(Archive& ar, Eigen::SparseMatrix<Theory::BigInteger>& sparse_matrix) {
-  Eigen::SparseMatrix<Theory::BigInteger>::Index rows = 0, cols = 0, outer_size = 0, inner_size = 0, non_zeros = 0;
+  Eigen::SparseMatrix<Theory::BigInteger>::Index rows = 0, cols = 0, non_zeros = 0, outer_size = 0, inner_size = 0;
   ar(rows);
   ar(cols);
+  ar(non_zeros);
   ar(outer_size);
   ar(inner_size);
-  ar(non_zeros);
   sparse_matrix.resize(rows, cols);
   sparse_matrix.makeCompressed();
   sparse_matrix.resizeNonZeros(non_zeros);
   for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i < non_zeros; ++i) {
     Util::Serialize::load(ar, sparse_matrix.valuePtr()[i]);
   }
-  ar(cereal::binary_data(sparse_matrix.outerIndexPtr(), outer_size * sizeof(Eigen::SparseMatrix<Theory::BigInteger>::StorageIndex)));
+  ar(cereal::binary_data(sparse_matrix.outerIndexPtr(), (outer_size + 1) * sizeof(Eigen::SparseMatrix<Theory::BigInteger>::StorageIndex)));
   ar(cereal::binary_data(sparse_matrix.innerIndexPtr(), non_zeros * sizeof(Eigen::SparseMatrix<Theory::BigInteger>::StorageIndex)));
   sparse_matrix.finalize();
+//
+//  std::cout << std::left;
+//  std::cout << std::setw(20) << "rows - cols:" << rows << " - " << cols << std::endl;
+//  std::cout << std::setw(20) << "non zeros:" << non_zeros << std::endl;
+//  std::cout << std::setw(20) << "outer size:" << outer_size << std::endl;
+//  std::cout << std::setw(20) << "inner size:" << inner_size << std::endl;
+//  std::cout << "values:" << std::endl;
+//  for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i < non_zeros; ++i) {
+//    std::cout << sparse_matrix.valuePtr()[i] << " ";
+//  }
+//  std::cout << std::endl;
+//  std::cout << "outer pointers:" << std::endl;
+//  for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i < outer_size; ++i) {
+//    std::cout << sparse_matrix.outerIndexPtr()[i] << " ";
+//  }
+//  std::cout << std::endl;
+//  std::cout << "inner pointers:" << std::endl;
+//  for (Eigen::SparseMatrix<Theory::BigInteger>::Index i = 0; i < non_zeros; ++i) {
+//    std::cout << sparse_matrix.innerIndexPtr()[i] << " ";
+//  }
+//
+//  std::cout << std::endl;
+//  std::cout << "what about nonzeros: " << sparse_matrix.nonZeros() << std::endl;
+//  std::cout << "------------------------------------------------" << std::endl;
+
 }
 
 } /* namespace Serialize */
